70f16604ba54e06696730cd7818f3169693d3fb8
wico <wico.wen@neweb.com.tw>
2023-05-18 17:03:11 +0800

    chore: tidy code

--- routes/api.php
+++ routes/api.php
@@ -23,18 +23,6 @@ use App\Http\Controllers\HomeController;
 #    return $request->user();
 #});
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('jwt.auth')->get('/user', function (Request $request) {
     $adminHelper = new AdminHelper();
     $user = $adminHelper->GetAuthUser();
