a54a3250ad58c2cff9652e7a0d06a89c977da9f8
wico <is@wico.cc>
2023-05-19 03:44:00 +0000

    change api prefix from "api" to "v1"

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,12 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
+            #Route::middleware('api')
+            #    ->prefix('api')
+            #    ->group(base_path('routes/api.php'));
+
             Route::middleware('api')
-                ->prefix('api')
+                ->prefix('v1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
