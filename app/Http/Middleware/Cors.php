99d3d7152bfeefa9927ed4fdb36eb5b2acab4c65
WicoWen <is@wico.cc>
2023-06-23 18:58:15 +0800

    debug: trying add cros header

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,8 @@
+<?php
+
+public function handle($request, Closure $next)
+{
+    return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,7 @@ class RouteServiceProvider extends ServiceProvider
             #    ->prefix('api')
             #    ->group(base_path('routes/api.php'));
 
-            Route::middleware('api')
+            Route::middleware(['api', 'cors'])
                 ->prefix('v1')
                 ->group(base_path('routes/api.php'));
 
